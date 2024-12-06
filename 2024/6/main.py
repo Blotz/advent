cbbbc5d32de6c3e58a9358d16f8ac1f0a4274543
Ferdinand Theil <55319277+Blotz@users.noreply.github.com>
2024-12-06 16:22:33 +0000

    simplify and cleanup code

--- 2024/6/main.py
+++ 2024/6/main.py
@@ -12,10 +12,30 @@ class Direction(Enum):
     EAST  = 2
     SOUTH = 3
     WEST  = 4
+    
+    def next(self):
+        if self == Direction.NORTH:
+            return Direction.EAST
+        elif self == Direction.EAST:
+            return Direction.SOUTH
+        elif self == Direction.SOUTH:
+            return Direction.WEST
+        elif self == Direction.WEST:
+            return Direction.NORTH
+
+dirs = {
+    Direction.NORTH: (-1, 0),
+    Direction.EAST:  (0, 1),
+    Direction.SOUTH: (1, 0),
+    Direction.WEST:  (0, -1)
+}
+
+def is_in_bounds(actor, shape):
+    return actor[0] >= 0 and actor[0] < shape[0] and actor[1] >= 0 and actor[1] < shape[1]
 
 async def main():
     puzzle = []
-    orig_actor = [0, 0]
+    orig_actor = (0, 0)
     orig_direction = Direction.NORTH
     
     async with aiofiles.open('input.txt', 'r') as file:
@@ -30,109 +50,58 @@ async def main():
                     x = len(row)
                     y = len(puzzle)
                     row.append(0)
-                    orig_actor = [y, x]
+                    orig_actor = (y, x)
             
             puzzle.append(row)            
 
-    orig_puzzle = np.array(puzzle, dtype=np.byte)
-    shape = orig_puzzle.shape
-    
-    # puzzle[actor[0], actor[1]] = 2
-    # print(puzzle)
-    
-    puzzle = np.copy(orig_puzzle)
-    actor = list(orig_actor)
+    puzzle = np.array(puzzle, dtype=np.byte)
+    shape = puzzle.shape
+    actor = orig_actor
     direction = orig_direction
     previous_positions = set()
     
     while True:
         previous_positions.add((actor[0], actor[1]))
         
-        if direction == Direction.SOUTH:
-            if actor[0] + 1 >= shape[0]:
-                break
-            elif puzzle[actor[0] + 1, actor[1]] == 1:
-                direction = Direction.WEST
-            else:
-                actor[0] += 1
-        elif direction == Direction.EAST:
-            if actor[1] + 1 >= shape[1]:
-                break
-            elif puzzle[actor[0], actor[1] + 1] == 1:
-                direction = Direction.SOUTH
-            else:
-                actor[1] += 1
-        elif direction == Direction.NORTH:
-            if actor[0] - 1 < 0:
-                break
-            elif puzzle[actor[0] - 1, actor[1]] == 1:
-                direction = Direction.EAST
-            else:
-                actor[0] -= 1
-        elif direction == Direction.WEST:
-            if actor[1] - 1 < 0:
-                break
-            elif puzzle[actor[0], actor[1] - 1] == 1:
-                direction = Direction.NORTH
-            else:
-                actor[1] -= 1
+        move = dirs[direction]
+        next = (actor[0] + move[0], actor[1] + move[1])
+        if not is_in_bounds(next, shape):
+            break
+        elif puzzle[next[0], next[1]] == 1:
+            direction = direction.next()
+        else:
+            actor = next
         
-    print(puzzle)
     print(len(previous_positions))
     
-    # previous_positions = set()
-    # previous_positions.add((actor[0], actor[1], direction))
     causes_loop = 0
     for idx in tqdm(previous_positions):
-        puzzle = np.copy(orig_puzzle)
-        puzzle[idx[0], idx[1]] = 1
-        actor = list(orig_actor)
+        actor = orig_actor
         direction = orig_direction
         directional_previous_position = set()
         
         while True:
-            # puzzle[actor[0], actor[1]] = 2
-            if (actor[0], actor[1], direction) in directional_previous_position:
+            if (direction, actor[0], actor[1]) in directional_previous_position:
                 causes_loop += 1
                 break
             else:
-                directional_previous_position.add((actor[0], actor[1], direction))
+                directional_previous_position.add((direction, actor[0], actor[1]))
             
+            move = dirs[direction]
+            next = (actor[0] + move[0], actor[1] + move[1])
+            if not is_in_bounds(next, shape):
+                break
+            elif puzzle[next[0], next[1]] == 1:
+                direction = direction.next()
+            elif next == (idx[0], idx[1]):
+                direction = direction.next()
+            else:
+                actor = next
             
-            if direction == Direction.SOUTH:
-                if actor[0] + 1 >= shape[0]:
-                    break
-                elif puzzle[actor[0] + 1, actor[1]] == 1:
-                    direction = Direction.WEST
-                else:
-                    actor[0] += 1
-            elif direction == Direction.EAST:
-                if actor[1] + 1 >= shape[1]:
-                    break
-                elif puzzle[actor[0], actor[1] + 1] == 1:
-                    direction = Direction.SOUTH
-                else:
-                    actor[1] += 1
-            elif direction == Direction.NORTH:
-                if actor[0] - 1 < 0:
-                    break
-                elif puzzle[actor[0] - 1, actor[1]] == 1:
-                    direction = Direction.EAST
-                else:
-                    actor[0] -= 1
-            elif direction == Direction.WEST:
-                if actor[1] - 1 < 0:
-                    break
-                elif puzzle[actor[0], actor[1] - 1] == 1:
-                    direction = Direction.NORTH
-                else:
-                    actor[1] -= 1
-            
-        # print(puzzle)    
     print(causes_loop)
     
 if __name__=='__main__':
     t1 = time.time()
     asyncio.run(main())
     t2 = time.time()
-    print(f"Execution time: {(t2-t1)*1000: .2f}ms")
+    print(f"Execution time: {(t2-t1): .2f}s")
