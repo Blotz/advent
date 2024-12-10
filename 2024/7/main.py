0d442010444f56ecc2b67473fd153bfc0cd749bc
Ferdinand Theil <55319277+Blotz@users.noreply.github.com>
2024-12-10 16:18:17 +0000

    2x speedup

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -10,10 +10,7 @@ async def main():
         async for line in file:
             line = line.strip().split(':')
             target = int(line[0])
-            numbers = list(map(int, line[1][1:].split(' ')))
-            
-            # print(f'Target: {target}')
-            # print(f'Numbers: {numbers}')
+            numbers = list(map(int, line[1].strip().split(' ')))
             
             if find_sum(target, numbers):
                 total += target
@@ -21,31 +18,28 @@ async def main():
     print(f'Total: {total}')
 
 def find_sum(target, numbers):
-    if not numbers:
-        return False
-    
-    numbers = numbers.copy()  # Copy the list to avoid modifying the original
-    
-    
-    n0 = numbers.pop(0)
-    n1 = numbers.pop(0)
+    n0 = numbers.pop(0)  # first num
+    n1 = numbers.pop(0)  # second num
     
     add = n0 + n1
     mul = n0 * n1
     cat = int(str(n0) + str(n1))
     
+    # base case.
     if len(numbers) == 0:
-        if add == target:
-            return True
-        
-        if mul == target:
-            return True
-        
-        if cat == target:
-            return True
+        if add == target: return True
+        if mul == target: return True
+        if cat == target: return True
 
         return False
     
+    # early return if our number is too big (optional)
+    if add > target: return False
+    if mul > target: return False
+    if cat > target: return False
+    
+    # add result to front of list
+    # and call func again
     return (find_sum(target, [add] + numbers) 
         or find_sum(target, [mul] + numbers)
         or find_sum(target, [cat] + numbers))
