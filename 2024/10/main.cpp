96d0da9b610b58b873d5f14e64c6ce7f9ba9a955
Ferdinand Theil <55319277+Blotz@users.noreply.github.com>
2024-12-10 16:47:00 +0000

    recombine task1 and task2

--- 2024/10/main.cpp
+++ 2024/10/main.cpp
@@ -24,9 +24,11 @@ bool is_inbound(int x, int y, int max_x, int max_y) {
     return x >= 0 && x < max_x && y >= 0 && y < max_y;
 }
 
-int follow_trail(vector<vector<int>> &grid, int x, int y) {
+int task1_trail(vector<vector<int>> &grid, int x, int y) {
     int current = grid[y][x];
     if (current == 9) {
+        // janky hack to stop us counting endings more than once
+        grid[y][x] = 0;  
         return 1;
     }
 
@@ -36,11 +38,31 @@ int follow_trail(vector<vector<int>> &grid, int x, int y) {
         int new_x = x + dirs[i][1];
         int new_y = y + dirs[i][0];
 
-        if (is_inbound(new_x, new_y, grid[0].size(), grid.size())) {
-            if (grid[new_y][new_x] == current+1) {
-                count += follow_trail(grid, new_x, new_y);
-            }
-        }
+        if (!is_inbound(new_x, new_y, grid[0].size(), grid.size())) { continue; }
+        if (grid[new_y][new_x] != current+1) { continue; }
+        count += task1_trail(grid, new_x, new_y);
+        // optional speedup 
+        grid[y][x] = 0;
+    }
+
+    return count;
+}
+
+int task2_trail(vector<vector<int>> &grid, int x, int y) {
+    int current = grid[y][x];
+    // base case
+    if (current == 9) { return 1; }
+
+    int count = 0;
+
+    for (int i=0; i < 4; i++) {
+        int new_x = x + dirs[i][1];
+        int new_y = y + dirs[i][0];
+
+        if (!is_inbound(new_x, new_y, grid[0].size(), grid.size())) { continue; }
+        if (grid[new_y][new_x] != current+1) { continue; }
+
+        count += task2_trail(grid, new_x, new_y);
     }
 
     return count;
@@ -71,26 +93,29 @@ int main() {
 
     // algorithm
     // print_grid(grid);
-    int total = 0;
-    int count;
-
+    int task1 = 0;
+    int task2 = 0;
+    vector<vector<int>> _grid;
 
     for (int y=0; y < grid.size(); y++) {
         for (int x=0; x < grid[y].size(); x++) {
             if (grid[y][x] == 0) {
-                count = follow_trail(grid, x, y);
-                cout << count << endl;
-                total += count;
+                // we need to copy because we edit grid with
+                // because of our hack. :P
+                _grid = grid;
+                task1 += task1_trail(_grid, x, y);
+                task2 += task2_trail(grid, x, y);
             }
         }
     }
 
-    cout << "Total: " << total << endl;
+    cout << "Task 1: " << task1 << endl;
+    cout << "Task 2: " << task2 << endl;
     
 
     // time c code
     auto t2 = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(t2-t1);
+    auto duration = chrono::duration_cast<chrono::microseconds>(t2-t1);
     cout << "Execution time: " << duration << endl;
     return 0;
 }
