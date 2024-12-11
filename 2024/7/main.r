56ad109362473634506edcb3ee04586e57749abb
Ferdinand Theil <55319277+Blotz@users.noreply.github.com>
2024-12-11 12:35:51 +0000

    rewrite using pipes

--- 2024/7/main.r
+++ 2024/7/main.r
@@ -1,24 +1,9 @@
 library(tidyverse)
-library(stringr)
-library(gtools)
-
-data <- data.frame("one"= readLines("input.txt"))
+library(stringr)  # str_strip
 
 start <- Sys.time()
 
-df <- data %>%
-  separate(one, into = c("result", "using"), sep = ": ")
-
-df$result <- as.numeric(df$result)
-df$match <- FALSE
-# using_lists <- str_split(df$using, " ")
-# convert to ints
-using_lists <- str_split(df$using, " ") %>% map(~as.numeric(.x))
-
-max_length <- max(sapply(using_lists, length))
-# count df$using rows
-
-find_sum <- function(result, numbers) {
+is_valid <- function(result, numbers) {
     n1 <- numbers[1]
     n2 <- numbers[2]
     numbers <- numbers[-c(1, 2)]
@@ -30,18 +15,36 @@ find_sum <- function(result, numbers) {
         return (result == add | result == mult)
     }
 
-    if (add < result) { if (find_sum(result, c(add, numbers))) { return (TRUE)} }
-    if (mult < result) { if (find_sum(result, c(mult, numbers))) { return (TRUE)} }
+    if (add < result) { 
+        if (is_valid(result, c(add, numbers))) {
+            return (TRUE)
+        }
+    }
+    if (mult < result) { 
+        if (is_valid(result, c(mult, numbers))) { 
+            return (TRUE)
+        } 
+    }
 
     return (FALSE)
 }
-total <- 0
-for(i in 1:nrow(df)){
-    if (find_sum(df$result[i], using_lists[[i]])) {
-        total <- total + df$result[i]
-    }
-}
 
-print(paste("result", total))
+file_path <- "input.txt"
+result <- read_lines(file_path) %>%
+    str_split(":") %>%
+    map(~ {
+        target <- .x[1] %>%
+            as.numeric()  # target exceeds integer range
+        numbers <- .x[2] %>%
+            str_trim() %>%
+            str_split(" ") %>%
+            map(~ as.numeric(.x)) %>%  # numbers excced integer range
+            unlist()
+
+        ifelse(is_valid(target, numbers), target, 0)
+    }) 
+
+
+print(paste("result", sum(unlist(result))))
 
 Sys.time()-start
